d2f1dff74e6a48b89e08c156f592809a9caf6328
Florian Demmer <fdemmer@gmail.com>
2016-02-23 18:53:40 +0100

    Add a sample model

--- models.py
+++ models.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
 {{ unicode_literals }}from django.db import models
+from django.utils.encoding import python_2_unicode_compatible, force_text
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
+# @python_2_unicode_compatible
+# class MyModel(models.Model):
+#     """
+#
+#     """
+#     class Meta:
+#         verbose_name = _('')
+#         verbose_name_plural = _('')
+#         ordering = ()
+#
+#     def __str__(self):
+#         return force_text(self.pk)
