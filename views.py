0a000a31d666a1d4a61b0adbb761807ea4d5e4f8
Florian Demmer <fdemmer@gmail.com>
2016-02-23 18:54:34 +0100

    Add sample detail view

--- views.py
+++ views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.views.generic import DetailView
+
 
 # Create your views here.
+# class MyView(DetailView):
+#     """
+#
+#     """
+#     model = MyModel
+#     pk_url_kwarg = 'pk'
+#     template_name = '{{ app_name }}/my.html'
