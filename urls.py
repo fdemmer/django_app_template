566501eec08766fa58bb609490002c96468b3dca
Florian Demmer <fdemmer@gmail.com>
2016-02-23 18:56:02 +0100

    Add urls.py

---
+++ urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+
+from .views import *
+
+
+# Add your urlpatterns here.
+urlpatterns = [
+#    url(r'^myview/(?P<pk>[0-9]+)/$', MyView.as_view(), name='myview'),
+]
