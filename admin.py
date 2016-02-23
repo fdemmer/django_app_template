57ddc61dc3a39cc41eafb57fdf72577af3dbc5f4
Florian Demmer <fdemmer@gmail.com>
2016-02-23 18:54:15 +0100

    Add sample model admin

--- admin.py
+++ admin.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
+
+# class MyModelAdmin(admin.ModelAdmin):
+#     list_display = ()
+#     list_filter = ()
+#     fieldsets = (
+#         (None, {
+#             'fields': (),
+#         }),
+#     )
+#     raw_id_fields = ()
+#     readonly_fields = ()
+#     inlines = []
+
+
 # Register your models here.
+# admin.site.register(MyModel, MyModelAdmin)
