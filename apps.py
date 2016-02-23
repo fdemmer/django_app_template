ab06f450c039ea69718216f87c1c6f734313df07
Florian Demmer <fdemmer@gmail.com>
2016-02-23 18:56:13 +0100

    Suggest using a verbose name for the app

--- apps.py
+++ apps.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 {{ unicode_literals }}from django.apps import AppConfig
 
 
 class {{ camel_case_app_name }}Config(AppConfig):
     name = '{{ app_name }}'
+    # verbose_name = ''
